a523b355b512c4e717d0b20ebbe2c6407e89ccd9
Mark Newman <mwnewman@umich.edu>
2023-10-23 16:19:33 -0400

    started adding Firebase

--- data/Actions.js
+++ data/Actions.js
@@ -1,34 +1,50 @@
 
+import { firebaseConfig } from '../Secrets';
 import { ADD_ITEM, UPDATE_ITEM, DELETE_ITEM } from "./Reducer";
 
+import { initializeApp } from 'firebase/app';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const addItem = (newText) => {
-  return {
-    type: ADD_ITEM,
-    payload: {
-      text: newText, 
-    }
+  return async (dispatch) => {
+    const docRef = await addDoc(collection(db, 'todos'), {text: newText});
+    const id = docRef.id;
+    dispatch({
+      type: ADD_ITEM,
+      payload: {
+        text: newText, 
+        key: id
+      }
+    });
   }
 }
 
 const updateItem = (item, newText) => {
-  return {
-    type: UPDATE_ITEM,
-    payload: {
-      key: item.key,
-      text: newText, 
-    }
+  return (dispatch) => {
+    dispatch({
+      type: UPDATE_ITEM,
+      payload: {
+        key: item.key,
+        text: newText, 
+      }
+    });
   }
 }
 
-
 const deleteItem = (item) => {
-  return {
-    type: DELETE_ITEM,
-    payload: {
-      key: item.key
-    }
+  return (dispatch) => {
+    dispatch({
+      type: DELETE_ITEM,
+      payload: {
+        key: item.key
+      }
+    })
   };
 }
+
 export {
   addItem, updateItem, deleteItem
 }
